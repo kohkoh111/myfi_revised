1b7e8b626caeea5c0bd43ae0a8dfb01e75fda5a2
Koki Takata <69852106+kohkoh111@users.noreply.github.com>
2021-03-13 22:12:52 +0900

    Update config.php

--- config/config.php
+++ config/config.php
@@ -6,13 +6,12 @@ $timezone = date_default_timezone_set("Asia/Tokyo");
 
 //$con = mysqli_connect("localhost", "root", "root", "social",8889); //Connection variable
 
-$db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
-$db['dbname'] = ltrim($db['path'], '/');
-$host = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
-$user = $db['user'];
-$password = $db['pass'];
-
-$con = mysqli_connect($host, $user, $pass, $db['dbname'])
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+$con = new mysqli($server, $username, $password, $db);
 
 if(mysqli_connect_errno())
 {
