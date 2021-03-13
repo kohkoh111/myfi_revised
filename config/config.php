536966cdc1310128ac4d1f04db43e022d7afc50c
Koki Takata <69852106+kohkoh111@users.noreply.github.com>
2021-03-13 22:05:26 +0900

    Update config.php

--- config/config.php
+++ config/config.php
@@ -4,7 +4,15 @@ session_start();
 
 $timezone = date_default_timezone_set("Asia/Tokyo");
 
-$con = mysqli_connect("localhost", "root", "root", "social",8889); //Connection variable
+//$con = mysqli_connect("localhost", "root", "root", "social",8889); //Connection variable
+
+$db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
+$db['dbname'] = ltrim($db['path'], '/');
+$host = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
+$user = $db['user'];
+$password = $db['pass'];
+
+$con = mysqli_connect($host, $user, $pass, $db['dbname'])
 
 if(mysqli_connect_errno())
 {
